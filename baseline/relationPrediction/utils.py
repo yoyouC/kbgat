0124f2d1ff30f595e51fe6d99b0f89191e22de07
Xuyou Cheng <Yoyoucheng@outlook.com>
2022-03-28 19:25:22 +0100

    add regul

--- baseline/relationPrediction/utils.py
+++ baseline/relationPrediction/utils.py
@@ -98,6 +98,9 @@ def hier_score(h, r, t, h_a, t_a):
 def simi_score(h, r, t, h_a, t_a):
     return t_a - h_a + (h - t).norm(1, dim=1)
 
+def regularization(h, r, t, regul_rate=1e-5):
+    return ((torch.mean(h ** 2) + torch.mean(t ** 2) + torch.mean(r ** 2)) / 3) * regul_rate
+
 class BatchCategoryDataset():
 
     class CategoryDataset():
