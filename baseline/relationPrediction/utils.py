81d792b8529c0e4032b2dd9c409585d9a6a94d27
Xuyou Cheng <Yoyoucheng@outlook.com>
2022-03-29 20:10:04 +0100

    add regularization

--- baseline/relationPrediction/utils.py
+++ baseline/relationPrediction/utils.py
@@ -134,3 +134,7 @@ def plot_grad_flow_low(named_parameters, parameters):
     plt.grid(True)
     plt.savefig('initial.png')
     plt.close()
+
+
+def regularization(h, r, t, regul_rate):
+    return ((torch.mean(h ** 2) + torch.mean(t ** 2) + torch.mean(r ** 2)) / 3) * regul_rate
